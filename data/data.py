49ed384a38d3762216b9dd719d8c305e07971be4
Brian Hockett <bhock0712@gmail.com>
2025-12-02 20:34:14 -0500

    Hi

--- data/data.py
+++ data/data.py
@@ -1,6 +1,33 @@
 import pybaseball as pb
 import pandas as pd
 
+def preprocess_statcast(data: pd.DataFrame):
+    df = data.copy()
+
+    # Descriptions that are considered swings
+    swing = ['hit_into_play', 'foul', 'swinging_strike', 'swinging_strike_blocked', 
+             'foul_tip', 'foul_bunt', 'missed_bunt', 'bunt_foul_tip']
+
+    # Descriptions that are considered whiffs
+    whiff = ['swinging_strike', 'swinging_strike_blocked', 'foul_tip']
+
+    # Columns for swing and whiff
+    df['swing'] = df['description'].isin(swing)
+    df['whiff'] = df['description'].isin(whiff)
+
+    # Columns for whether the pitch was in or out of the zone (1-9 are strikes, 10+ are balls)
+    df['in_zone'] = df['zone'] < 10
+    df['out_zone'] = df['zone'] > 10
+
+    # Column for whether a swing was a chase
+    df['chase'] = df['swing'] & ~df['in_zone']
+
+    # Convert break values to inches
+    df['pfx_z'] = df['pfx_z'] * 12
+    df['pfx_x'] = df['pfx_x'] * 12
+
+    return df
+
 # Define the date ranges by month
 months = [
     ('2025-03-30', '2025-03-31'),
@@ -9,8 +36,7 @@ months = [
     ('2025-06-01', '2025-06-30'),
     ('2025-07-01', '2025-07-31'),
     ('2025-08-01', '2025-08-31'),
-    ('2025-09-01', '2025-09-30'),
-    ('2025-10-01', '2025-10-01')
+    ('2025-09-01', '2025-09-30')
 ]
 
 all_data = []
@@ -23,10 +49,9 @@ for start, end in months:
 # Concatenate all chunks into one DataFrame
 statcast_data_2025 = pd.concat(all_data, ignore_index=True)
 
+# Apply preprocessing
+statcast_data_2025 = preprocess_statcast(statcast_data_2025)
+
 # Save to Parquet
 statcast_data_2025.to_parquet('./statcast_data_2025.parquet', index=False)
-print("Saved full 2025 Statcast data to data/statcast_data_2025.parquet")
-
-# Save to csv
-# statcast_data_2025.to_csv('../project_work/statcast_data_2025.csv', index=False)
-# print("Saved full 2025 Statcast data to data/statcast_data_2025.csv")
+print("Saved processed 2025 Statcast data to ./statcast_data_2025.parquet")
--- preprocessing.py
+++
@@ -1,4 +0,0 @@
-import pybaseball as pb
-import pandas as pd
-import numpy as np
-
